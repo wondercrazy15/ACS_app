f1b70f48c6d897b172754777d3d585f02d167b6b
imac <harshp8513@gmail.com>
2022-10-04 18:57:21 +0530

    - implementede leader board not complete - Resolved issue of community screen.

--- ACS_App/ACS_App/Views/LeaderboardView.xaml.cs
+++ ACS_App/ACS_App/Views/LeaderboardView.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
@@ -8,9 +8,25 @@ namespace ACS_App.Views
 {
     public partial class LeaderboardView : ContentPage
     {
+        //List<string> TeamList = new List<string>();
         public LeaderboardView()
         {
             InitializeComponent();
+            //string listItem1 = "Gilbert Oppong";
+            //string listItem2 = "Joseph Edusei";
+            //string listItem3 = "Frank Odoom";
+            //string listItem4 = "Kyereh Bismark";
+
+            //TeamList.Add(listItem1);
+            //TeamList.Add(listItem2);
+            //TeamList.Add(listItem3);
+            //TeamList.Add(listItem4);
+            //TeamList.Add(listItem4);
+            //TeamList.Add(listItem4);
+            //TeamList.Add(listItem4);
+            //TeamList.Add(listItem4);
+
+            //Name_list.ItemsSource = TeamList;
         }
         protected override void OnAppearing()
         {
@@ -18,12 +34,44 @@ namespace ACS_App.Views
 
             var safeAreaInset = On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets();
             BgStack.Padding = new Thickness(0, safeAreaInset.Top, 0, 0);
-            bgStackBottom.Padding = new Thickness(0, 0, 0, safeAreaInset.Bottom);
+
+            List<ListClass> listItems = new List<ListClass>();
+            listItems.Add(new ListClass { Index = 1 , Name = "Gilbert Oppong", Points = 123456});
+            listItems.Add(new ListClass { Index = 2, Name = "Joseph Edusei", Points = 85178 });
+            listItems.Add(new ListClass { Index = 3, Name = "Frank Odoom", Points = 25987 });
+            listItems.Add(new ListClass { Index = 4, Name = "Kyereh Bismark", Points = 41200 });
+            listItems.Add(new ListClass { Index = 5, Name = "Kyereh Bismark", Points = 72000 });
+            listItems.Add(new ListClass { Index = 6, Name = "Kyereh Bismark", Points = 85657 });
+            listItems.Add(new ListClass { Index = 7, Name = "Kyereh Bismark", Points = 36487 });
+            listItems.Add(new ListClass { Index = 8, Name = "Kyereh Bismark", Points = 12879 });
+            Name_list.ItemsSource = listItems;
         }
 
         void backArrow(System.Object sender, System.EventArgs e)
         {
             Navigation.PopModalAsync();
         }
+
+        void TapGestureRecognizer_Tapped_left(System.Object sender, System.EventArgs e)
+        {
+            Individuals_2_frame.BackgroundColor = Color.FromHex("#722BAD");
+            Teams_2_frame.BackgroundColor = Color.White;
+            //myPageStack.IsVisible = true;
+            //myBadgesStack.IsVisible = false;
+        }
+
+        void TapGestureRecognizer_Tapped_Right(System.Object sender, System.EventArgs e)
+        {
+            Individuals_2_frame.BackgroundColor = Color.White;
+            Teams_2_frame.BackgroundColor = Color.FromHex("#722BAD");
+            //myPageStack.IsVisible = false;
+            //myBadgesStack.IsVisible = true;
+        }
+    }
+    public class ListClass
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
     }
 }
--- ACS_App/ACS_App/Views/UsersView.xaml.cs
+++ ACS_App/ACS_App/Views/UsersView.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
@@ -15,13 +15,19 @@ namespace ACS_App.Views
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
+            var dwidth = DeviceDisplay.MainDisplayInfo.Width;
+            //var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            //var orientation = mainDisplayInfo.Orientation;
+            FirstlableWidth.WidthRequest = dwidth * 0.25;
+            SecondlableWidth.WidthRequest = dwidth * 0.25;
+            ThirdlableWidth.WidthRequest = dwidth * 0.25;
+            FourlableWidth.WidthRequest = dwidth * 0.25;
             var safeAreaInset = On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets();
             BgStack.Padding = new Thickness(0, safeAreaInset.Top, 0, 0);
             bgStackBottom.Padding = new Thickness(0, 0, 0, safeAreaInset.Bottom);
         }
 
-        void OnTapped(System.Object sender, System.EventArgs e)
+        void CompetitionTab(System.Object sender, System.EventArgs e)
         {
             Navigation.PushModalAsync(new Xamarin.Forms.NavigationPage(new LeaderboardView()));
         }
